1b12cb90e918369cd5c709aa146acb4d63f58392
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2024-09-22 19:02:31 -0400

    added outlet to root

--- apps/frontend/src/containers/root.tsx
+++ apps/frontend/src/containers/root.tsx
@@ -1,9 +1,11 @@
 import Header from '@components/Header';
+import { Outlet } from 'react-router-dom';
 
 const Root: React.FC = () => {
   return (
     <>
       <Header />
+      <Outlet />
     </>
   );
 };
