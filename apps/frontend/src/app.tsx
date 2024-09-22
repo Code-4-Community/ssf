fa613b35270ccdd9f32a9ac1ed52cf2e7e5a705b
Sam Schumacher <42777208+sam-schu@users.noreply.github.com>
2024-09-22 12:40:37 -0400

    Remove test page

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -4,17 +4,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import apiClient from '@api/apiClient';
 import Root from '@containers/root';
 import NotFound from '@containers/404';
-import Test from '@containers/test';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <NotFound />,
-  },
-  {
-    path: '/test',
-    element: <Test />,
   },
 ]);
 
