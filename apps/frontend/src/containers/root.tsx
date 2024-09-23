7760096988e60881f457453169bf2de086effe89
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2024-09-22 20:26:13 -0400

    created and added header component to root

--- apps/frontend/src/containers/root.tsx
+++ apps/frontend/src/containers/root.tsx
@@ -1,5 +1,12 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../../components/Header';
 const Root: React.FC = () => {
-  return <>Welcome to scaffolding!</>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
 
 export default Root;
