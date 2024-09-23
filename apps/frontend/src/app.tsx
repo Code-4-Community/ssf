d31a1ec11f6458ce824fc9a7e0d0099cea08dc9d
Sam Schumacher <42777208+sam-schu@users.noreply.github.com>
2024-09-22 20:03:24 -0400

    Add routes for new pages

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -4,12 +4,39 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import apiClient from '@api/apiClient';
 import Root from '@containers/root';
 import NotFound from '@containers/404';
+import LandingPage from '@containers/landingPage';
+import PantryOverview from '@containers/pantryOverview';
+import PantryPastOrders from '@containers/pantryPastOrders';
+import Pantries from '@containers/pantries';
+import Orders from '@containers/orders';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <NotFound />,
+    children: [
+      {
+        path: '/landing-page',
+        element: <LandingPage />,
+      },
+      {
+        path: '/pantry-overview',
+        element: <PantryOverview />,
+      },
+      {
+        path: '/pantry-past-orders',
+        element: <PantryPastOrders />,
+      },
+      {
+        path: '/pantries',
+        element: <Pantries />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
+    ],
   },
 ]);
 
