bbccb08fc38af7aa56585fc0e9e2ede3051b2797
Garamon <ollietoh@gmail.com>
2024-10-04 03:27:05 -0400

    fix theme colors

--- apps/frontend/src/theme.ts
+++ apps/frontend/src/theme.ts
@@ -3,9 +3,9 @@ import { extendTheme } from '@chakra-ui/react';
 const colors = {
   white: '#fff',
   black: '#000',
-  blue: '#FC987C',
+  blue: '#2B5061',
   red: '#CC3538',
   yellow: '#F89E19',
-  cyan: '#1C95A5',
+  cyan: '##2795A5',
 };
 export const theme = extendTheme({ colors });
