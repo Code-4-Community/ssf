77c0aa746ed365fe134fdb7ad554556353586d3e
Sam Schumacher <42777208+sam-schu@users.noreply.github.com>
2024-10-06 19:25:29 -0400

    Remove auth

--- apps/backend/src/pantries/pantries.module.ts
+++ apps/backend/src/pantries/pantries.module.ts
@@ -2,22 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PantriesController } from './pantries.controller';
 import { PantriesService } from './pantries.service';
-import { UsersService } from '../users/users.service';
 import { Pantry } from './pantry.entity';
-import { JwtStrategy } from '../auth/jwt.strategy';
-import { CurrentUserInterceptor } from '../interceptors/current-user.interceptor';
-import { AuthService } from '../auth/auth.service';
-import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Pantry])],
+  imports: [TypeOrmModule.forFeature([Pantry])],
   controllers: [PantriesController],
-  providers: [
-    UsersService,
-    PantriesService,
-    AuthService,
-    JwtStrategy,
-    CurrentUserInterceptor,
-  ],
+  providers: [PantriesService],
 })
 export class PantriesModule {}
