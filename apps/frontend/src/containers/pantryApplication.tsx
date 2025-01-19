06e0b572bc80e6bcb3e1dd307cd6b37fb8df2c9b
Sam Schumacher <42777208+sam-schu@users.noreply.github.com>
2025-01-19 14:30:31 -0500

    Add PantryApplication page component

---
+++ apps/frontend/src/containers/pantryApplication.tsx
@@ -0,0 +1,13 @@
+import { Center } from '@chakra-ui/react';
+import PantryApplicationForm from '@components/forms/pantryApplicationForm';
+import React from 'react';
+
+const PantryApplication: React.FC = () => {
+  return (
+    <Center>
+      <PantryApplicationForm />
+    </Center>
+  );
+};
+
+export default PantryApplication;
