75361f203b6f8440cc4295a1e81c526cbd57e0cc
Justin Wang <Juwang110@gmail.com>
2025-03-26 18:15:50 -0400

    make sign out button component instead of on landing page

--- apps/frontend/src/containers/landingPage.tsx
+++ apps/frontend/src/containers/landingPage.tsx
@@ -1,17 +1,5 @@
-import { Button } from '@chakra-ui/react';
-import { signOut } from 'aws-amplify/auth';
-
 const LandingPage: React.FC = () => {
-  const handleSignOut = async () => {
-    await signOut();
-  };
-
-  return (
-    <>
-      Landing page
-      <Button onClick={handleSignOut}> sign out</Button>
-    </>
-  );
+  return <>Landing page</>;
 };
 
 export default LandingPage;
