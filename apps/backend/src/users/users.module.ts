50b0bc362806a111d5eb5005cdcf4cd70df9b4a9
amywng <147568742+amywng@users.noreply.github.com>
2025-10-09 16:58:25 -0400

    volunteer backend updates minus tests

--- apps/backend/src/users/users.module.ts
+++ apps/backend/src/users/users.module.ts
@@ -6,10 +6,12 @@ import { User } from './user.entity';
 import { JwtStrategy } from '../auth/jwt.strategy';
 import { CurrentUserInterceptor } from '../interceptors/current-user.interceptor';
 import { AuthService } from '../auth/auth.service';
+import { Assignments } from '../volunteerAssignments/volunteerAssignments.entity';
+import { Pantry } from '../pantries/pantries.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([User, Assignments, Pantry])],
+  exports: [UsersService, TypeOrmModule],
   controllers: [UsersController],
   providers: [UsersService, AuthService, JwtStrategy, CurrentUserInterceptor],
 })
