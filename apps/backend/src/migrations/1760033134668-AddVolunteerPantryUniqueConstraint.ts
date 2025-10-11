e54b6a32cdb83c137d1c1a56d1ffa9c0016c7c75
amywng <147568742+amywng@users.noreply.github.com>
2025-10-11 15:34:42 -0400

    review comments

--- apps/backend/src/migrations/1760033134668-AddVolunteerPantryUniqueConstraint.ts
+++ apps/backend/src/migrations/1760033134668-AddVolunteerPantryUniqueConstraint.ts
@@ -6,22 +6,14 @@ export class AddVolunteerPantryUniqueConstraint1760033134668
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             ALTER TABLE volunteer_assignments DROP COLUMN assignment_id;
-        `);
 
-    await queryRunner.query(`
             ALTER TABLE volunteer_assignments
             ADD PRIMARY KEY (volunteer_id, pantry_id);
-        `);
 
-    await queryRunner.query(`
             ALTER TABLE volunteer_assignments DROP CONSTRAINT IF EXISTS fk_volunteer_id;
-        `);
 
-    await queryRunner.query(`
             ALTER TABLE volunteer_assignments DROP CONSTRAINT IF EXISTS fk_pantry_id;
-        `);
 
-    await queryRunner.query(`
             ALTER TABLE volunteer_assignments
             ADD CONSTRAINT fk_volunteer_id FOREIGN KEY (volunteer_id) REFERENCES users(user_id) ON DELETE CASCADE,
             ADD CONSTRAINT fk_pantry_id FOREIGN KEY (pantry_id) REFERENCES pantries(pantry_id) ON DELETE CASCADE;
@@ -31,17 +23,11 @@ export class AddVolunteerPantryUniqueConstraint1760033134668
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             ALTER TABLE volunteer_assignments DROP CONSTRAINT fk_volunteer_id;
-        `);
 
-    await queryRunner.query(`
             ALTER TABLE volunteer_assignments DROP CONSTRAINT fk_pantry_id;
-        `);
 
-    await queryRunner.query(`
             ALTER TABLE volunteer_assignments DROP CONSTRAINT volunteer_assignments_pkey;
-        `);
 
-    await queryRunner.query(`
             ALTER TABLE volunteer_assignments ADD COLUMN assignment_id SERIAL PRIMARY KEY;
         `);
   }
