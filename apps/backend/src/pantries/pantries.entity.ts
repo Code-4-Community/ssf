b7c6466d5f4ee206bdec776d31237b0a9eca6859
amywng <147568742+amywng@users.noreply.github.com>
2025-11-02 18:46:23 -0500

    review comments and changing db schema

--- apps/backend/src/pantries/pantries.entity.ts
+++ apps/backend/src/pantries/pantries.entity.ts
@@ -63,11 +63,17 @@ export class Pantry {
   })
   refrigeratedDonation: RefrigeratedDonation;
 
-  @Column({
-    name: 'reserve_food_for_allergic',
-    type: 'enum',
-    enum: ReserveFoodForAllergic,
-    enumName: 'reserve_food_for_allergic_enum',
+  @Column({ name: 'accept_food_deliveries', type: 'boolean' })
+  acceptFoodDeliveries: boolean;
+
+  @Column({ name: 'delivery_window_instructions', type: 'text', nullable: true })
+  deliveryWindowInstructions?: string;
+
+  @Column({ 
+    name: 'reserve_food_for_allergic', 
+    type: 'enum', 
+    enum: ReserveFoodForAllergic, 
+    enumName: 'reserve_food_for_allergic_enum' 
   })
   reserveFoodForAllergic: string;
 
