0855f8f84c5dc5c09e307a1a098ef1e5f4316f64
Dalton Burkhart <daltonburkhart.75@gmail.com>
2025-11-23 01:32:57 -0500

    Fixed ssf rep id

--- apps/backend/src/migrations/1731171000000-UpdatePantryUserFields.ts
+++ apps/backend/src/migrations/1731171000000-UpdatePantryUserFields.ts
@@ -11,7 +11,7 @@ export class UpdatePantryUserFields1731171000000 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE pantries RENAME COLUMN pantry_user_id TO pantry_representative_id;
-       ALTER TABLE pantries ADD COLUMN ssf_representative_id INT NOT NULL;
+       ALTER TABLE pantries ADD COLUMN ssf_representative_id INT;
        ALTER TABLE pantries ADD CONSTRAINT fk_ssf_representative_id 
        FOREIGN KEY (ssf_representative_id) REFERENCES users(user_id);`,
     );
