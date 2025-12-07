d3cb538cbfa4b06bfae032871918ef095333dfe3
Dalton Burkhart <daltonburkhart.75@gmail.com>
2025-12-07 10:34:09 -0500

    Fixed migration

--- apps/backend/src/migrations/1764811878152-RemoveMultipleVolunteerTypes.ts
+++ apps/backend/src/migrations/1764811878152-RemoveMultipleVolunteerTypes.ts
@@ -1,42 +1,46 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class RemoveMultipleVolunteerTypes1764811878152
-  implements MigrationInterface
-{
+export class RemoveMultipleVolunteerTypes1764811878152 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            ALTER TABLE users
-            DROP COLUMN role;
-            DROP TYPE IF EXISTS users_role_enum;
+      ALTER TABLE users ALTER COLUMN role DROP DEFAULT;
 
-            CREATE TYPE "users_role_enum" AS ENUM (
-                'admin',
-                'volunteer',
-                'pantry',
-                'food_manufacturer'
-            );
+      CREATE TYPE "users_role_enum_new" AS ENUM (
+        'admin',
+        'volunteer',
+        'pantry',
+        'food_manufacturer'
+      );
 
-            ALTER TABLE users
-            ADD COLUMN role users_role_enum NOT NULL DEFAULT 'volunteer';
-        `);
+      ALTER TABLE users
+      ALTER COLUMN role
+      TYPE users_role_enum_new
+      USING role::text::users_role_enum_new;
+      DROP TYPE users_role_enum;
+      ALTER TYPE users_role_enum_new
+      RENAME TO users_role_enum;
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            ALTER TABLE users
-            DROP COLUMN role;
-            DROP TYPE IF EXISTS users_role_enum;
+      ALTER TABLE users ALTER COLUMN role DROP DEFAULT;
 
-            CREATE TYPE "users_role_enum" AS ENUM (
-                'admin',
-                'lead_volunteer',
-                'standard_volunteer',
-                'pantry',
-                'food_manufacturer'
-            );
+      CREATE TYPE "users_role_enum_old" AS ENUM (
+        'admin',
+        'lead_volunteer',
+        'standard_volunteer',
+        'pantry',
+        'food_manufacturer'
+      );
 
-            ALTER TABLE users
-            ADD COLUMN role users_role_enum NOT NULL DEFAULT 'standard_volunteer';
-        `);
+      ALTER TABLE users
+      ALTER COLUMN role
+      TYPE users_role_enum_old
+      USING role::text::users_role_enum_old;
+      DROP TYPE users_role_enum;
+      ALTER TYPE users_role_enum_old
+      RENAME TO users_role_enum;
+    `);
   }
 }
