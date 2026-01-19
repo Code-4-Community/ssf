6aea768ce5a2f9c74a61acc2997728d30f1bd92e
Dalton Burkhart <daltonburkhart.75@gmail.com>
2026-01-18 22:13:20 -0500

    Fixed module importing

--- apps/backend/src/orders/order.module.ts
+++ apps/backend/src/orders/order.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersController } from './order.controller';
 import { Order } from './order.entity';
 import { OrdersService } from './order.service';
 import { AllocationModule } from '../allocations/allocations.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order]), AllocationModule],
+  imports: [TypeOrmModule.forFeature([Order]), AllocationModule, forwardRef(() => AuthModule)],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
--- apps/backend/src/users/users.module.ts
+++ apps/backend/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { PantriesModule } from '../pantries/pantries.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => PantriesModule)],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => PantriesModule), forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
