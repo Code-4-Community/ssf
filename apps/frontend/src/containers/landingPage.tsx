7ac8fc5d02951aabcbe5bdfaab1ab7ce621f3555
Dalton Burkhart <daltonburkhart.75@gmail.com>
2026-02-01 00:36:12 -0500

    Final commit

--- apps/frontend/src/containers/landingPage.tsx
+++ apps/frontend/src/containers/landingPage.tsx
@@ -3,11 +3,9 @@ import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const LandingPage: React.FC = () => {
   const { user } = useAuthenticator((context) => [context.user]);
-  console.log('LandingPage user:', user);
   return (
     <>
       Landing page
-      {/* Only show if user exists */}
       {user && <SignOutButton />}
     </>
   );
