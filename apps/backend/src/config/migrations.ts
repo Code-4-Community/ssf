dee5203bc2d95eef74f3c6fd99d2686171ed88b3
amywng <147568742+amywng@users.noreply.github.com>
2026-02-06 15:26:08 -0500

    add migration

--- apps/backend/src/config/migrations.ts
+++ apps/backend/src/config/migrations.ts
@@ -25,6 +25,7 @@ import { RemoveUnusedStatuses1764816885341 } from '../migrations/1764816885341-R
 import { UpdatePantryFields1763762628431 } from '../migrations/1763762628431-UpdatePantryFields';
 import { PopulateDummyData1768501812134 } from '../migrations/1768501812134-populateDummyData';
 import { RemovePantryFromOrders1769316004958 } from '../migrations/1769316004958-RemovePantryFromOrders';
+import { UpdateOrderEntity1769990652833 } from '../migrations/1769990652833-UpdateOrderEntity';
 
 const schemaMigrations = [
   User1725726359198,
@@ -54,6 +55,7 @@ const schemaMigrations = [
   RemoveUnusedStatuses1764816885341,
   PopulateDummyData1768501812134,
   RemovePantryFromOrders1769316004958,
+  UpdateOrderEntity1769990652833,
 ];
 
 export default schemaMigrations;
