2b6004fbb4d1c589050a964e966ddca0a670f744
Dalton Burkhart <daltonburkhart.75@gmail.com>
2026-02-06 00:00:32 -0500

    Fixed ci flow

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,7 +9,6 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-// temp comment
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
