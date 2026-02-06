52f356f6789d6a2c3cb4e4c16e29df4b95b79bd6
amywng <147568742+amywng@users.noreply.github.com>
2026-02-06 10:25:07 -0500

    add comment about cron pattern

--- apps/backend/src/donations/donations.scheduler.ts
+++ apps/backend/src/donations/donations.scheduler.ts
@@ -8,7 +8,13 @@ export class DonationsSchedulerService {
 
   constructor(private readonly donationService: DonationService) {}
 
-  @Cron('0 9 * * *') // Runs every day at 9 AM
+  // cron pattern:
+  // supported: *, ranges/#'s (e.g. 1-3, 5), steps (e.g. */2)
+  //    * indicates the method should be run every _ unit of time
+  //    range/# indicates method should be run on the _ unit of time/between the _ and _ unit of time
+  //    step indicates the method should be run every _ unit of time
+  // fields in order: second, minute, hour, day of month, month, day of week
+  @Cron('0 30 10 * * *') // Runs every day at 10:30 AM
   async handleDailyRecurringDonations() {
     this.logger.log('Running daily donation reminder cron job');
     await this.donationService.handleRecurringDonations();
