263f655bd150ecd5d3007fe01293b3db18bc741e
Dalton Burkhart <137221425+dburkhart07@users.noreply.github.com>
2026-03-01 13:20:31 -0500

    [SSF-145] - Cognito integration account creation (#113)
    * Final commit
    
    * Final commit
    
    * Final commit fr this time
    
    * Final changes
    
    * Fixed test
    
    * Fixed auth cycle
    
    * Made submission route for FMs public

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -41,7 +41,6 @@ export class UsersController {
 
   @Post('/')
   async createUser(@Body() createUserDto: userSchemaDto): Promise<User> {
-    const { email, firstName, lastName, phone, role } = createUserDto;
-    return this.usersService.create(email, firstName, lastName, phone, role);
+    return this.usersService.create(createUserDto);
   }
 }
