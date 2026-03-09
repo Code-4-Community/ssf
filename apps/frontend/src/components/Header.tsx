a1a5fdc12f5be98eed820b36ef6bda936cbe2f63
amywng <147568742+amywng@users.noreply.github.com>
2026-03-08 17:31:14 -0700

    link header to homepage

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import SignOutButton from './signOutButton';
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from '@chakra-ui/react';
 
 const Header = () => {
   const { user } = useAuthenticator((context) => [context.user]);
+  const navigate = useNavigate();
 
   return (
     <div style={{ display: 'flex', gap: '16px' }}>
-      <h1>Securing Safe Food</h1>
+      <Link onClick={() => navigate('/')}>
+        <h1>Securing Safe Food</h1>
+      </Link>
+
       {user && <SignOutButton size="sm" variant="outline"></SignOutButton>}
     </div>
   );
