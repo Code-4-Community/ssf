783487f78d09139fc1828910f4c34e36316d0704
amywng <147568742+amywng@users.noreply.github.com>
2026-03-08 19:59:49 -0700

    switch to react router link

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import SignOutButton from './signOutButton';
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { useNavigate } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import { Link } from '@chakra-ui/react';
 
 const Header = () => {
   const { user } = useAuthenticator((context) => [context.user]);
-  const navigate = useNavigate();
 
   return (
     <div style={{ display: 'flex', gap: '16px' }}>
-      <Link onClick={() => navigate('/')}>
-        <h1>Securing Safe Food</h1>
+      <Link asChild>
+        <RouterLink to="/">Securing Safe Food</RouterLink>
       </Link>
 
       {user && <SignOutButton size="sm" variant="outline"></SignOutButton>}
