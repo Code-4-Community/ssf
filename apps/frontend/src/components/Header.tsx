0e9b080bd72d9c828c96d0087710a279adef827e
Amy Wang <147568742+amywng@users.noreply.github.com>
2026-03-08 20:18:39 -0700

    [SSF-161] link header to homepage (#122)
    * link header to homepage
    
    * switch to react router link

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import SignOutButton from './signOutButton';
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Link } from '@chakra-ui/react';
 
 const Header = () => {
   const { user } = useAuthenticator((context) => [context.user]);
 
   return (
     <div style={{ display: 'flex', gap: '16px' }}>
-      <h1>Securing Safe Food</h1>
+      <Link asChild>
+        <RouterLink to="/">Securing Safe Food</RouterLink>
+      </Link>
+
       {user && <SignOutButton size="sm" variant="outline"></SignOutButton>}
     </div>
   );
