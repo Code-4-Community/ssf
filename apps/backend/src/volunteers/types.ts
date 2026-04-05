312b46e3f66c726d47d332ab10508124d31666fd
Amy Wang <147568742+amywng@users.noreply.github.com>
2026-04-05 13:54:21 -0400

    [SSF-176] volunteer order endpoints (#143)
    * volunteer order endpoints
    
    * clean up and add tests
    
    * comments
    
    * comments
    
    * comments
    
    * sam comments

--- apps/backend/src/volunteers/types.ts
+++ apps/backend/src/volunteers/types.ts
@@ -1,3 +1,26 @@
+import { OrderStatus } from '../orders/types';
 import { User } from '../users/users.entity';
 
 export type Assignments = Omit<User, 'pantries'> & { pantryIds: number[] };
+
+export type VolunteerOrder = {
+  orderId: number;
+  status: OrderStatus;
+  createdAt: Date;
+  shippedAt: Date | null;
+  deliveredAt: Date | null;
+  pantryName: string;
+  assignee: OrderAssignee;
+  actionCompletion?: VolunteerActionCompletion;
+};
+
+export type VolunteerActionCompletion = {
+  confirmDonationReceipt: boolean;
+  notifyPantry: boolean;
+};
+
+export type OrderAssignee = {
+  id: number;
+  firstName: string;
+  lastName: string;
+};
