02a741406f80234c0403eed4268c593144f1d626
Dalton Burkhart <daltonburkhart.75@gmail.com>
2026-04-12 20:02:28 -0400

    added icon in login page

---
+++ apps/frontend/src/components/AuthHeader.tsx
@@ -0,0 +1,25 @@
+import { Box } from '@chakra-ui/react';
+
+const AuthHeader: React.FC = () => (
+  <Box
+    h="74px"
+    w="full"
+    bg="white.core"
+    borderBottomWidth="1px"
+    borderBottomColor="neutral.100"
+    display="flex"
+    alignItems="center"
+    pl="26px"
+    flexShrink={0}
+  >
+    <Box w="40px" h="40px">
+      <img
+        src="/favicon.ico"
+        alt="SSF"
+        style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+      />
+    </Box>
+  </Box>
+);
+
+export default AuthHeader;
